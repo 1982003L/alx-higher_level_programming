05ff7b326d43543e2536504680e8024061e6dd0c
Abdel <elarabawyabdo03@gmail.com>
2023-11-06 14:05:59 -0800

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,85 +1,47 @@
 #include "lists.h"
 
 /**
- * calculate_n_nodes - Calculate the number of nodes in the linked list.
- * @temp: Pointer to a pointer to the head of the linked list.
- * Return: Number of nodes in the linked list.
- */
-int calculate_n_nodes(listint_t **temp)
-{
-	listint_t *current;
-	unsigned int n;
-
-	if (temp == NULL || *temp == NULL)
-		return (0);
-
-	current = *temp;
-	n = 0;
-
-	while (current != NULL)
-	{
-		current = current->next;
-		n++;
-	}
-
-	return (n);
-}
-
-/**
- * compare_nodes - Compare values of two nodes.
- * @current: Pointer to the current node.
- * @current_n: Number of the current node.
- * Return: 1 if the nodes have the same value, 0 otherwise.
- */
-int compare_nodes(listint_t *current, int current_n)
-{
-	int i = 0;
-	listint_t *trace = current;
-
-	if (current_n == 1)
-		return (1);
-	else if (current_n == 2)
-	{
-		if (current->n == current->next->n)
-			return (1);
-		else
-			return (0);
-	}
-	while (i < current_n - 1)
-	{
-		trace = trace->next;
-		i++;
-	}
-	if (current->n == trace->n)
-		return (1);
-	else
-		return (0);
-}
-
-/**
- * is_palindrome - Check if a linked list is a palindrome.
- * @head: Pointer to a pointer to the head of the linked list.
- * Return: 1 if the linked list is a palindrome, 0 otherwise.
+ * is_palindrome - check if linked list is palindrome
+ * @head: pointer to pointer
+ * Return: (1) if palindrome, otherwise (0)
  */
 int is_palindrome(listint_t **head)
 {
-	int n = calculate_n_nodes(head);
-	listint_t *current;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *f_half = *head;
+	listint_t *s_half;
+	listint_t *prev = NULL;
+	listint_t *cur, *next;
 
-	current = *head;
-
-	if (n == 0)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (current != NULL)
+	while (fast && fast->next)
 	{
-		if (compare_nodes(current, n))
-		{
-			current = current->next;
-			n -= 2;
-		}
-		else
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	cur = slow;
+
+	while (cur)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	s_half = prev;
+
+
+	while (s_half)
+	{
+		if (f_half->n != s_half->n)
 			return (0);
+
+		f_half = f_half->next;
+		s_half = s_half->next;
 	}
 
 	return (1);
