d72ae0f1b206c8f33732a94e231e1dd813828940
Abdel <elarabawyabdo03@gmail.com>
2024-03-04 16:38:45 -0800

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
+
 
 if __name__ == '__main__':
-    from urllib.request import urlopen
-    url = 'https://alx-intranet.hbtn.io/status'
-    with urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print(f'\t- type: {type(html)}')
-        print(f'\t- content: {html}')
-        print(f'\t- utf8 content: {html.decode("utf-8")}')
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
